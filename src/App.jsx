774987f6d45fee778b4b4e9aee8b9ed9bdee1d62
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-09-23 08:47:02 +0600

    create student component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-
-import './App.css'
-
+import "./student.css"
 function App() {
-
-
   return (
     <>
       <div>
-        <p>Hi my name is mehedi hasan</p>
+        <Student></Student>
+        <Student></Student>
+      </div>
+    </>
+  )
+}
+
+function Student(){
+  const studentOne = {
+    name : 'Mehedi Hasan',
+    class: '8th',
+    birth:'03-01-2003',
+    roll : '2344',
+    address: 'Rajshahi Bangladesh'
+
+  }
+
+  return(
+    <>
+      <div className="bground">
+        <h1>Name: {studentOne.name}</h1>
+        <h2>Class: {studentOne.class}</h2>
+        <h2>D.O.Birth: {studentOne.birth}</h2>
+        <h2>Roll: {studentOne.roll}</h2>
+        <h2>Address: {studentOne.address}</h2>
       </div>
     </>
   )
 }
 
 export default App
+ 
