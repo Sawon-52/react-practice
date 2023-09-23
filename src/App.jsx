30ca101b1b37c9354f4781515c7346a924a94761
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-09-23 10:15:50 +0600

    passing properties, clear props concept

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,36 @@ import "./student.css"
 function App() {
   return (
     <>
-      <div>
-        <Student></Student>
-        <Student></Student>
-      </div>
+      <div className="container">
+        <Student name='Sarmin Sultana' position='10th' birth='03-02-2005' roll='222' address='Rajshahi Bangladesh'></Student>
+        <Student name='Abdullah' position='10th' birth='03-02-2005' roll='222' address='Rajshahi Bangladesh'></Student>
+        <Student name='Solaiman' position='10th' birth='03-02-2005' roll='222' address='Rajshahi Bangladesh'></Student>
+        <Student name='Karim' position='10th' birth='03-02-2005' roll='222' address='Rajshahi Bangladesh'></Student>
+      </div> 
     </>
   )
 }
 
-function Student(){
-  const studentOne = {
-    name : 'Mehedi Hasan',
-    class: '8th',
-    birth:'03-01-2003',
-    roll : '2344',
-    address: 'Rajshahi Bangladesh'
+const {name, position, birth, roll, address } = {
+  name : 'Mehedi Hasan',
+  position: '9th',
+  birth:'03-01-2004',
+  roll : '2344',
+  address: 'Rajshahi Bangladesh'
+}
 
-  }
+// create function component abd it is student
+function Student({name, position, birth, roll, address}){
 
+  
   return(
     <>
       <div className="bground">
-        <h1>Name: {studentOne.name}</h1>
-        <h2>Class: {studentOne.class}</h2>
-        <h2>D.O.Birth: {studentOne.birth}</h2>
-        <h2>Roll: {studentOne.roll}</h2>
-        <h2>Address: {studentOne.address}</h2>
+        <h1>Name: {name}</h1>
+        <h2>Class: {position}</h2>
+        <h2>D.O.Birth: {birth}</h2>
+        <h2>Roll: {roll}</h2>
+        <h2>Address: {address}</h2>
       </div>
     </>
   )
